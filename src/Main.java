a9d10d3dabcd3a27d0787cddc22f819facb704ca
isa nono <e225722@ie.u-ryukyu.ac.jp[alias]>
2022-12-10 19:39:40 +0900

    step2 1st

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try{
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            //例外状況のエラーメッセージを出力
+            System.out.println("例外が発生しました。null以外で初期化してください。");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("処理が終了しました"); 
     }
 }
